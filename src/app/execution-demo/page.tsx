2b24f3aa720f67d5ed1500d77a0fb33460f00588
Abdallah Torkamani <abdellahturkmani@gmail.com>
2026-08-19 23:27:57 +0100

    feat: add execution loop prototype

---
+++ src/app/execution-demo/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/layout/app-shell";
+import { ExecutionDemo } from "@/components/execution-demo/execution-demo";
+
+export default function ExecutionDemoPage() {
+  return (
+    <AppShell title="Execution Demo">
+      <ExecutionDemo />
+    </AppShell>
+  );
+}
